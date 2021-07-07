002687aee0a52a9c34ba862f4ef019d8064aa197
CamiloMar7 <jukamaro20@gmail.com>
2021-07-06 19:08:59 -0500

    mensajes GUIA conexion con github

--- src/App.java
+++ src/App.java
@@ -3,3 +3,21 @@ public class App {
         System.out.println("Hola interneeeet !!");
     }
 }
+
+//poner consola en gitbash
+//en consola poner: ls  (enter)
+//en consola poner: git init  (enter)
+//en consola poner: git config --global user.email "jukamaro20@gmail.com"  (enter)
+//en consola poner: git config --global user.name "CamiloMar7"  (enter)
+//en consola poner: git remote add origin https://github.com/CamiloMar7/Ciclo2MisionTic.git
+//en consola poner: git push origin master  (enter)
+//en consola poner: git push origin master  (enter)
+//** SE REALIZA CONEXIÓN CON SERVIDOR, ENLACE CON INTERNET PAGINA GITHUB**/
+
+//** EN VERSIONADOR, 3 puntos y hacer stage STAGE ALL */
+//** EN VERSIONADOR, 3 puntos y hacer commit con comentario COMMIT ALL*/
+//** EN VERSIONADOR, 3 puntos y hacer push, pull -> PUSH TO... */
+// y se selecciona link de github repositorio
+
+
+
